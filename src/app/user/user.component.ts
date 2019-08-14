6ed74636d8034ea3fd4945bc4582a983d060f209
xsenia <belkinasan@yandex.ru>
2019-08-14 21:45:30 +0500

    Вывод переменных в шаблон

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  public user = {
+    name: "John"
+  };
+
   constructor() { }
+
+  random() {
+    return Math.random();
+  }
 
   ngOnInit() {
   }
